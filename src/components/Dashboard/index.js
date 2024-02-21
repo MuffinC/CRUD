24cb6385323f56167e65675c16c89f66b1e38861
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2024-02-21 11:12:43 +0800

    del
    Delete functionality

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,7 +6,7 @@ import Table from './Table';
 import Add from './Add';
 import Edit from './Edit';
 
-import {collection, getDocs} from "firebase/firestore";
+import {collection, getDocs, doc, deleteDoc} from "firebase/firestore";
 import {db} from '../../config/firestore'
 
 const Dashboard = ({ setIsAuthenticated }) => {
@@ -44,8 +44,9 @@ const getEmployees = async () =>{
     }).then(result => {
       if (result.value) {
         const [employee] = employees.filter(employee => employee.id === id);
-
-        // TODO delete document
+        
+        //function to remove data from firebaste
+        deleteDoc(doc(db,'employees',id))
 
         Swal.fire({
           icon: 'success',
